cca36e41f4a6cfb37f7ea4cb2eaa9f7aae50507b
vtoanb <vtoanb@gmail.com>
2016-10-16 12:40:19 +0700

    update controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,19 +14,21 @@ class OrdersController < ApplicationController
   	end
 
   	food_items = params[:food].split('/')
-  	# total bill
-  	total = 0
-  	food_items.each do |e|
-  	  i = e.to_i
-      @user.orders.create(food_id: i, num_of_food: 1) if e.to_i > 0
-      # calculate total bill
-      total += Food.find(i).price if i > 0
-  	end
-    @user.total_bill = total
-    # saving @usr
-    @user.save
-    # redirect to order
-  	redirect_to action: 'show', id: @user.id
+    if food_items
+    	# total bill
+    	total = 0
+    	food_items.each do |e|
+    	  i = e.to_i
+        @user.orders.create(food_id: i, num_of_food: 1) if e.to_i > 0
+        # calculate total bill
+        total += Food.find(i).price if i > 0
+    	end
+      @user.total_bill = total
+      # saving @usr
+      @user.save
+      # redirect to order
+    	redirect_to action: 'show', id: @user.id
+    end
   end
 
   def show
